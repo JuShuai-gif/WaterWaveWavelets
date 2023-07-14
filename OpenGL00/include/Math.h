1a29e6ba633c3556e7d98025d138ff0f32121660
ghr <208967048@qq.com>
2023-07-14 22:11:55 +0800

    add math module

---
+++ OpenGL00/include/Math.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <eigen/Eigen/Dense>
+
+template <class T> struct ValueTraits {
+
+    static constexpr T zero() {
+        if constexpr (std::is_base_of_v<Eigen::DenseBase<T>, T>) {
+            return T::Zero();
+        }
+        else {
+            return 0;
+        }
+    }
+};
--- include/Math.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ArrayAlgebra.h"
-#include "Integration.h"
-
-#include "DomainInterpolation.h"
-#include "Interpolation.h"
-
-constexpr int pos_modulo(int n, int d) { return (n % d + d) % d; }
